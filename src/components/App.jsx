c546ae62ae0b2bf1427942f13efcfcfbfebb69f1
Leandro Arturi <lea.arturi@gmail.com>
2022-12-30 17:18:45 -0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+function App() {
+   const [count, setCount] = useState(0);
+
+   return (
+      <div className='App'>
+         <p>Hola</p>
+         <p>Mundo</p>
+      </div>
+   );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
